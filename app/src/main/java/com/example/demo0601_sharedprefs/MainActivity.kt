14125c72e4cf2744039d4e7b8842752b0d6b0133
xiaoxiong1006 <758063761@qq.com>
2020-09-06 18:20:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demo0601_sharedprefs/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.demo0601_sharedprefs
+
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    // Current count
+    private var mCount = 0
+
+    // Current background color
+    private var mColor = 0
+
+    // Key for current count
+    private val COUNT_KEY = "count"
+
+    // Key for current color
+    private val COLOR_KEY = "color"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Handles the onClick for the background color buttons. Gets background
+     * color of the button that was clicked, and sets the TextView background
+     * to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    fun changeBackground(view: View) {
+        val color = (view.background as ColorDrawable).color
+        count_textview!!.setBackgroundColor(color)
+        mColor = color
+    }
+
+    /**
+     * Handles the onClick for the Count button. Increments the value of the
+     * mCount global and updates the TextView.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    fun countUp(view: View?) {
+        mCount++
+        count_textview!!.text = String.format("%s", mCount)
+    }
+
+    /**
+     * Handles the onClick for the Reset button. Resets the global count and
+     * background variables to the defaults and resets the views to those
+     * default values.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    fun reset(view: View?) {
+        // Reset count
+        mCount = 0
+        count_textview!!.text = String.format("%s", mCount)
+
+        // Reset color
+        mColor = ContextCompat.getColor(
+            this,
+            R.color.default_background
+        )
+        count_textview!!.setBackgroundColor(mColor)
+    }
+}
